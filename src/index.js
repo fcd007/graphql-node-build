f929f2b774c84e28e365f2f17760b8fd61a64d96
Claudeilton Dantas <fcd007@hotmail.com>
2020-09-23 20:35:45 -0300

    add schema and rootValue

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const MessageSchema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+const schemaRoot = {
+  message: () => "Hello World",
+};
+
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: "",
+    schema: MessageSchema,
+    rootValue: schemaRoot,
     graphiql: true,
     pretty: true,
   })
